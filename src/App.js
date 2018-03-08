77981143f6e5177ded34f5cd7b3515e85dc46115
Jason Brown <browniefed@gmail.com>
2018-03-08 09:03:34 -0800

    Add setState reducer code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
 import React, { Component } from "react";
 
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+
+const reducer = action => (state, props) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        value: state.value + 1,
+      };
+    case DECREMENT:
+      return {
+        value: state.value - 1,
+      };
+    default:
+      return null;
+  }
+};
+
 class App extends Component {
+  state = {
+    value: 0,
+  };
+  increment = () => {
+    this.setState(
+      reducer({
+        type: INCREMENT,
+      }),
+    );
+  };
+  decrement = () => {
+    this.setState(
+      reducer({
+        type: DECREMENT,
+      }),
+    );
+  };
   render() {
-    return <div />;
+    return (
+      <div>
+        <div>{this.state.value}</div>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
   }
 }
 
