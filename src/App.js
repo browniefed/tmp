a9c5f22f784cd425c29620cd61efe5350799aa60
Jason Brown <browniefed@gmail.com>
2018-03-12 18:11:41 -0700

    Add code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
+import "./app.css";
+import yup from "yup";
+
+const optionalRequiredSchema = yup.object().shape({
+  optionalObject: yup.lazy(value => {
+    if (value !== undefined) {
+      return yup.object().shape({
+        otherData: yup.string().required(),
+      });
+    }
+    return yup.mixed().notRequired();
+  }),
+});
 
 class App extends Component {
+  state = {};
   render() {
-    return <div />;
+    const valid = optionalRequiredSchema.isValidSync(this.state);
+
+    return (
+      <div className="container">
+        <div className="text" style={{ backgroundColor: valid ? "green" : "red" }}>
+          Valid Data: {""+valid}
+        </div>
+        <button onClick={() => this.setState({ optionalObject: {} })}>Add Optional</button>
+      </div>
+    );
   }
 }
 
