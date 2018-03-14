d3852f5f969223632a310890ab6614d59c49c4ba
Jason Brown <browniefed@gmail.com>
2018-03-14 09:31:20 -0700

    Import downup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { line } from "d3-shape";
 import { scaleLinear } from "d3-scale";
 import { data } from "./data";
+import { data as downUpData } from "./down_up";
 
 const width = 500;
 const height = 300;
