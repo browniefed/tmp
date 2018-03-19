35e6ea5d3fe20f373227e9e2159474c977fed974
Jason Brown <browniefed@gmail.com>
2018-03-19 14:17:04 -0700

    Add points

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React, { Component } from "react";
-import { line, curveStep } from "d3-shape";
+import { line } from "d3-shape";
 import { scaleLinear } from "d3-scale";
 import { data } from "./data";
 
 const width = 500;
 const height = 300;
+const padding = 10;
 
 const xSelector = d => d.x;
 const ySelector = d => d.y;
+
 const xScale = scaleLinear()
-  .range([0, width])
+  .range([padding, width - padding])
   .domain([0, 10]);
 
 const yScale = scaleLinear()
-  .range([height, 0])
+  .range([height - padding, padding])
   .domain([0, 500]);
 
 class App extends Component {
+  state = {
+    selected: null,
+  };
+  onMouseMove = e => {
+    const index = Math.round(xScale.invert(e.clientX), 0);
+    this.setState({
+      selected: index,
+    });
+  };
   render() {
     const path = line()
       .x(d => xScale(xSelector(d)))
@@ -24,13 +35,14 @@ class App extends Component {
 
     return (
       <div>
-        <svg width={width} height={height}>
+        <svg width={width} height={height} onMouseMove={this.onMouseMove}>
           <path d={path(data)} stroke="#ff6347" strokeWidth={3} fill="none" />
-          {data.map(d => {
+          {data.map((d, i) => {
             const yPoint = yScale(ySelector(d));
             const xPoint = xScale(xSelector(d));
+            const r = this.state.selected === i ? 10 : 5;
             return (
-              <circle cx={xPoint} cy={yPoint} r="3" stroke="#fff" strokeWidth={2} fill="#ff6347" />
+              <circle cx={xPoint} cy={yPoint} r={r} stroke="#fff" strokeWidth={2} fill="#ff6347" />
             );
           })}
         </svg>
