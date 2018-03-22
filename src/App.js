74df6b0c93af1ca3bd3e25861c1caa9c017e3433
Jason Brown <browniefed@gmail.com>
2018-03-22 08:58:27 -0700

    Add in code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React, { Component } from "react";
 
+import { Switch, Route, Link } from "react-router-dom";
+
+const Profile = () => <div>You're on the Profile Tab</div>;
+const Comments = () => <div>You're on the Comments Tab</div>;
+const Contact = () => <div>You're on the Contact Tab</div>;
+
 class App extends Component {
   render() {
-    return <div />;
+    const { path } = this.props.match;
+
+    return (
+      <div>
+        Hey welcome home!
+        <div>
+          <Link to={`${path}/profile`}>Profile</Link>
+          <Link to={`${path}/comments`}>Comments</Link>
+          <Link to={`${path}/contact`}>Contact</Link>
+        </div>
+        <div>
+          <Switch>
+            <Route path={`${path}/profile`} component={Profile} />
+            <Route path={`${path}/comments`} component={Comments} />
+            <Route path={`${path}/contact`} component={Contact} />
+          </Switch>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/home" component={App} />
+      <Redirect from="/" to="/home" />
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("root"));
