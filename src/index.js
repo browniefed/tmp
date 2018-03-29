ab62c2a75c2791d1ebbeae1b060c686cc899ee12
Jason Brown <browniefed@gmail.com>
2018-03-29 16:56:58 -0700

    Add

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+const routes = (
+  <BrowserRouter>
+    <Switch>
+    </Switch>
+  </BrowserRouter>
+)
+
 ReactDOM.render(<App />, document.getElementById("root"));
