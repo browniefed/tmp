9f2a2eaa96c8aa0d3bfca9b4d4132e04ce014a8e
Jason Brown <browniefed@gmail.com>
2018-03-30 08:27:15 -0700

    Add modal route using state if no state render inline

--- src/App.js
+++ src/App.js
@@ -2,27 +2,41 @@ import React, { Component } from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import "./app.css";
 
-const Profile = () => <div>You're on the Profile Tab</div>;
-const Comments = () => <div>You're on the Comments Tab</div>;
-const Contact = () => <div>You're on the Contact Tab</div>;
+const Home = () => <div>You're on the Home Tab</div>;
+const Photo = ({ location }) => {
+  const { state = {} } = location;
+  const { modal } = state;
+  return (
+    <div className={modal ? "modal" : undefined}>
+      {modal && <Link to="/">Close</Link>}
+      <p>You're looking at a photo!</p>
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
-    const { path } = this.props.match;
-   
     return (
       <div>
         <h1>Hey welcome home!</h1>
         <div className="links">
-          <Link to={`${path}`} className="link">Profile</Link>
-          <Link to={`${path}/comments`} className="link">Comments</Link>
-          <Link to={`${path}/contact`} className="link">Contact</Link>
+          <Link to="/" className="link">
+            Home
+          </Link>
+          <Link
+            to={{
+              pathname: "/photo",
+              state: { modal: true },
+            }}
+            className="link"
+          >
+            View Photo
+          </Link>
         </div>
         <div className="tabs">
           <Switch>
-            <Route path={`${path}`} exact component={Profile} />
-            <Route path={`${path}/comments`} component={Comments} />
-            <Route path={`${path}/contact`} component={Contact} />
+            <Route path="/" exact component={Home} />
+            <Route path="/photo" component={Photo} />
           </Switch>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const routes = (
   <BrowserRouter>
     <Switch>
-      <Route path="/home" component={App} />
-      <Redirect from="/" to="/home" />
+      <Route path="/" component={App} />
     </Switch>
   </BrowserRouter>
 );
