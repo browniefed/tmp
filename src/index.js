756e01e705a60513a80078a19e471330d13c03de
Jason Brown <browniefed@gmail.com>
2018-03-30 08:16:42 -0700

    Add code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 const routes = (
   <BrowserRouter>
     <Switch>
+      <Route path="/home" component={App} />
+      <Redirect from="/" to="/home" />
     </Switch>
   </BrowserRouter>
-)
+);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(routes, document.getElementById("root"));
