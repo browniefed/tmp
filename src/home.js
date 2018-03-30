21582f9803d87ad6512c66f017c5af170599fe06
Jason Brown <browniefed@gmail.com>
2018-03-30 09:05:22 -0700

    Add code

--- src/home.js
+++ src/home.js
@@ -1,12 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Home extends Component {
   render() {
-    return (
-      <div>
-        You're home!
-      </div>
-    );
+    return <div>You're home!</div>;
   }
 }
 
--- src/profile.js
+++ src/profile.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Prompt } from "react-router-dom";
 
 class Profile extends Component {
+  state = {
+    name: "",
+  };
   render() {
     return (
       <div>
-        Nice looking profile!
+        <Prompt
+          when={!!this.state.name}
+          message={location => `Are you sure you want to go to ${location.pathname}`}
+        />
+        <div>
+          <div>Nice looking profile! What's your name?</div>
+          <input value={this.state.name} onChange={e => this.setState({ name: e.target.value })} />
+        </div>
       </div>
     );
   }
