3f073eb587e123e1d3503d1c647a9882a98d6650
Jason Brown <browniefed@gmail.com>
2018-04-01 09:54:52 -0700

    Add matched code

--- src/App.js
+++ src/App.js
@@ -2,52 +2,71 @@ import React, { Component } from "react";
 import { Switch, Route, Link, matchPath } from "react-router-dom";
 import "./app.css";
 
-const Profile = () => <div>You're on the Profile Tab</div>;
-const Comments = () => <div>You're on the Comments Tab</div>;
-const Contact = () => <div>You're on the Contact Tab</div>;
+const Profile = ({ match, loading }) => {
+  if (loading) return <div>Loading...</div>;
+  return <div>You're on the Profile {match.params.profileId}</div>;
+};
+const SelectProfile = () => <div>Select a Profile</div>;
+
+const getParams = pathname => {
+  const matchProfile = matchPath(pathname, {
+    path: `/profile/:profileId`,
+  });
+  return (matchProfile && matchProfile.params) || {};
+};
 
 class App extends Component {
-
-  componentWillReceiveProps(nextProps) {
-    
-  }
-  
-  render() {
-    const { path } = this.props.match;
+  state = {
+    loading: false,
+  };
+  componentDidUpdate(prevProps, prevState) {
     const { pathname } = this.props.location;
+    const { pathname: prevPathname } = prevProps.location;
 
-    const profileMatched = matchPath(pathname, {
-      path: `${path}`,
-      exact: true,
-    });
+    const currentParams = getParams(pathname);
+    const prevParams = getParams(prevPathname);
 
-    const commentsMatched = matchPath(pathname, {
-      path: `${path}/comments`,
-    });
+    if (currentParams.profileId && currentParams.profileId !== prevParams.profileId) {
+      clearTimeout(this.timeout);
+      this.setState({
+        loading: true,
+      });
+      setTimeout(() => {
+        this.setState({
+          loading: false,
+        });
+      }, 500);
+    }
+  }
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+  }
 
-    const contactMatched = matchPath(pathname, {
-      path: `${path}/contact`,
-    });
+  render() {
+    console.log(this.props.match);
 
     return (
       <div>
-        <h1>Hey welcome home!</h1>
         <div className="links">
-          <Link to={`${path}`} className="link">
-            Profile
+          <Link to="/profile" className="link">
+            Home
           </Link>
-          <Link to={`${path}/comments`} className="link">
-            Comments
+          <Link to="/profile/1" className="link">
+            Profile 1
           </Link>
-          <Link to={`${path}/contact`} className="link">
-            Contact
+          <Link to="/profile/2" className="link">
+            Profile 2
           </Link>
         </div>
         <div className="tabs">
           <Switch>
-            <Route path={`${path}`} exact component={Profile} />
-            <Route path={`${path}/comments`} component={Comments} />
-            <Route path={`${path}/contact`} component={Contact} />
+            <Route path="/profile" exact component={SelectProfile} />
+            <Route
+              path="/profile/:profileId"
+              render={props => {
+                return <Profile {...props} loading={this.state.loading} />;
+              }}
+            />
           </Switch>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 const routes = (
   <BrowserRouter>
     <Switch>
-      <Route path="/home" component={App} />
-      <Redirect from="/" to="/home" />
+      <Route path="/profile" component={App} />
+      <Redirect from="/" to="/profile" />
     </Switch>
   </BrowserRouter>
 );
